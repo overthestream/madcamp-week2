0608ca5895a1d97cabd5e5aa2a133cc6b9039b09
overthestream <njhnjh02@naver.com>
2022-01-06 19:25:25 +0900

    socket test

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ const http = require('http');
 const server = require('./app');
 const socket = require('./socket');
 
-const PORT = 443;
+const PORT = 80;
 
 const httpServer = http.createServer(server).listen(PORT, () => {
   console.log(`server start on port: ${PORT}`);
