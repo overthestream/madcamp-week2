eb2c5b3589fd757a5d2a89f1efda17127e644eff
overthestream <njhnjh02@naver.com>
2022-01-06 20:08:32 +0900

    message test

--- src/socket.js
+++ src/socket.js
@@ -4,8 +4,20 @@ const webSocket = server => {
   const socket = socketIO(server);
   console.log(`now server can response socket.io request.`);
   socket.on('connection', req => {
-    console.log('HIHIHI');
     console.log('connection info : ', req.request.connection._peername);
+
+    socket.on('messageS', message => {
+      console.dir(message);
+      console.log('message 이벤트를 받았습니다.');
+
+      if (message.recepient === 'ALL') {
+        // 나를 포함한 모든 클라이언트에게 메시지 전달
+        console.dir(
+          '나를 포함한 모든 클라이언트에게 message 이벤트를 전송합니다.'
+        );
+        io.sockets.emit('messageC', message);
+      }
+    });
   });
 };
 
