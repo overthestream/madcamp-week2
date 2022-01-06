77a004e1d2e4a5ab0b04ad060fc3a2429ad7c0b7
overthestream <njhnjh02@naver.com>
2022-01-06 19:39:23 +0900

    찐찐막

--- src/socket.js
+++ src/socket.js
@@ -3,7 +3,7 @@ const socketIO = require('socket.io');
 const webSocket = server => {
   const socket = socketIO(server);
   console.log(`now server can response socket.io request.`);
-  socket.on('connection', req => {
+  socket.on('connect', req => {
     console.log('HIHIHI');
     console.log('connection info : ', req.request.connection._peername);
   });
