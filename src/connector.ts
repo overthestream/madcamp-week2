06141fa42cc588ebb3499fcbfc3e77d4de65d5ee
overthestream <njhnjh02@naver.com>
2022-01-07 19:56:08 +0900

    add dotenv

--- src/connector.ts
+++ src/connector.ts
@@ -1,11 +1,11 @@
 const { Pool } = require('pg');
 
 const pool = new Pool({
-  user: 'postgres',
-  host: 'localhost',
-  database: 'mbti',
-  password: '0129',
-  port: 5432,
+  user: process.env.DB_USER || 'postgres',
+  host: process.env.DB_HOST || 'localhost',
+  database: process.env.DB_NAME || 'mbti',
+  password: process.env.DB_PW || '0129',
+  port: process.env.DB_PORT || 5432,
 });
 
 const queryGenerator = async (queryStr: string, queryVal: Array<string>) => {
--- src/oauth.ts
+++ src/oauth.ts
@@ -1,2 +1,1 @@
-const myCode =
-  'thMNTWsyBxCd57svZsEwp9v3HbButEOPYiMkpfZVFqsG8Ra-mch7KtqJxvJh3npyeXYB7gopcJ8AAAF-M5lHdQ';
+const myCode = process.env.MYCODE;
